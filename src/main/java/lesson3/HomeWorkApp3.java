2779c1f66e4913e1121d51cf5a06fcddcb8fcde7
Assemgul <assemgul9@mail.ru>
2021-10-03 19:47:14 +0600

    lesson3

--- src/main/java/lesson3/HomeWorkApp3.java
+++ src/main/java/lesson3/HomeWorkApp3.java
@@ -8,9 +8,18 @@ import java.util.Scanner;
 public class HomeWorkApp3 {
     private static Scanner scanner = new Scanner(System.in);
 
+
     public static void main(String[] args) {
+
+
         fifthTask();
         nextTask();
+        sixTask();
+        taskTask3();
+        taskTask2();
+        taskTask();
+        seventhTask(new int[]{23, 23, 45, 56, 11, 2, 1, 9, 17, 4});
+        eighthTask(new int[]{1, 2, 3, 4, 5,}, 1);
     }
 
     public static void fifthTask() {
@@ -58,20 +67,80 @@ public class HomeWorkApp3 {
     public static void taskTask() {
         int[][] arr2 = new int[5][5];
         for (int i = 0; i < arr2.length; i++) {
-            for (int j = 0, x = arr2.length - 1; j < arr2.length; j++, x--) {
-                if (i == j) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (i == j || i == arr2.length - 1 - j) {
                     arr2[i][j] = 1;
-                    arr2[i][x] = 1;
                 }
                 System.out.print(arr2[i][j] + " ");
             }
             System.out.println();
         }
-
     }
-    public static void sixTask(){
-        int[]arr4=new int[]{123, 24, 47, 89, 90,23};
+
+    public static void sixTask() {
+        int[] arr4 = new int[]{0, 24, 47, 89, 90, 23};
+        int max = arr4[0];
+        int min = arr4[0];
+        for (int i = 0; i < arr4.length; i++) {
+            if (arr4[i] > max) {
+                max = arr4[i];
+            }
+            for (int j = 0; j < arr4.length; j++) {
+
+                if (arr4[i] < min) {
+                    min = arr4[i];
+                }
+            }
+        }
+        System.out.println("Максимальное число " + max + " Минимальное число " + min);
+    }
+
+    public static boolean seventhTask(int[] arr5) {
+
+        for (int i = 0; i < arr5.length; i++) {
+            int sumRight = 0;
+            int sumLeft = 0;
+
+            for (int j = 0; j < i; j++) {
+                sumRight += arr5[j];
+            }
+            for (int j = i; j < arr5.length; j++) {
+                sumLeft += arr5[j];
+
+                if (sumRight == sumLeft)
+
+                    return true;
+            }
+            System.out.println(sumRight=sumLeft);
+
+        }
+        return false;
+    }
+
+    public static void eighthTask(int[] arr6, int shift) {
+
+        int arrLength = arr6.length;
+        if (shift > 0) {
+            for (int i = 0; i < shift; i++) {
+                int temp = arr6[arrLength - 1];
+                for (int j = arrLength - 1; j > 0; j--) {
+                    arr6[j] = arr6[j - 1];
+                }
+                arr6[0] = temp;
+            }
+        }
+        if (shift < 0) {
+
+            for (int i = 0; i > shift; i--) {
+                int temp = arr6[0];
+                for (int m = 0; m < arrLength - 1; m++) {
+                    arr6[m] = arr6[m + 1];
+                }
+                arr6[arrLength - 1] = temp;
+            }
 
 
+
+        }System.out.print(Arrays.toString(arr6));
     }
 }
